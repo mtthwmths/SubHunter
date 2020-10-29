228fed0604a2e0e9ad3e00f5031c6ef7daa1d6a3
Matt Mathis <mtthwmths@gmail.com>
2020-10-28 23:06:48 -0500

    Adds first few chapters of code
    Mostly adds comments and the outline of subhunter.java file. some debugging
    log calls to show what function is being called.

--- app/src/main/java/com/mtthwmths/subhunter/SubHunter.java
+++ app/src/main/java/com/mtthwmths/subhunter/SubHunter.java
@@ -3,6 +3,16 @@ package com.mtthwmths.subhunter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.Display;
+import android.util.Log;
+import android.widget.ImageView;
+import java.util.Random;
 
 public class SubHunter extends android.app.Activity {
 
@@ -15,29 +25,58 @@ public class SubHunter extends android.app.Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("Debugging", "In onCreate");
+        newGame();
+        draw();
+
     }
 
     /*
     This code will execute when a new game needs to be started.
     It will happen when the app is first started and after the player wins a game.
      */
+    void newGame(){
+        Log.d("Debugging","In newGame");
+
+    }
 
     /*
     Here we will do all the drawing.
     The grid lines, the HUD and the touch indicator.
      */
+    void draw() {
+        Log.d("Debugging", "In draw");
+    }
 
     /*
     This part of the code will handle detecting that the player has tapped the screen.
      */
+    @Override
+    public boolean onTouchEvent (MotionEvent motionEvent) {
+        Log.d("Debugging", "In onTouchEvent");
+        takeShot();
+        return true;
+    }
 
     /*
     The code here will execute when the player taps the screen.
     It will calculate the distance from the sub and decide a hit or miss.
      */
+    void takeShot () {
+        Log.d("Debugging", "In takeShot");
+        draw();
+
+    }
 
     // This code says "BOOM!"
+    void boom() {
+
+    }
 
     // This code prints the debugging text
+    void printDebuggingText () {
+
+    }
     
 }
